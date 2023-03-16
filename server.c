77fbb1bcb65c49fbcf9fe37b40ffb772dcc07bf9
Maimuna Zaheer <68902873+itserror404@users.noreply.github.com>
2023-03-17 00:25:25 +0400

    Update server.c

--- server.c
+++ server.c
@@ -30,8 +30,8 @@
 struct User {
     char user[50];
     char password[50];
-    char address[50]; // this could be the IP address 
-    int is_authenticated;
+    char address[50]; // the IP address 
+    int is_authenticated; // for authentication for LIST STOR RETR
     int client_fd; 
 };
 
@@ -45,7 +45,7 @@ int user(char *username, int client_fd) {
 
     for (int i = 0; i < num_users; i++) {
         if (strcmp(userlist[i].user, username) == 0) {
-            userlist[i].is_authenticated = 0; // reset authentication status
+            userlist[i].is_authenticated = 0; // set authentication 0
             userlist[i].client_fd = client_fd; // save client
             send(client_fd, "331 Username OK, need password.\n", strlen("331 Username OK, need password.\n"), 0);
             return 0;
@@ -78,10 +78,10 @@ int pass(char *password, int client_fd) {
 
 
 
-
+// cwd
 int cwd(char *path, int client_fd){
 
-	if (chdir(path) == 0){
+	if (chdir(path) == 0){ // change dir
 		char message[256];
 		snprintf(message, sizeof(message), "200 directory changed to %s\n", path);
 		send(client_fd, message, strlen(message), 0);
@@ -97,7 +97,7 @@ int cwd(char *path, int client_fd){
 
 int pwd(char *path, int client_fd){
 
-	if (getcwd(path, sizeof(path)) == 0){
+	if (getcwd(path, sizeof(path)) == 0){ // get path of curr dir
 		char message[256];
 		snprintf(message, sizeof(message), "550 No such file or directory\n");
 		send(client_fd, message, strlen(message), 0);
@@ -111,113 +111,20 @@ int pwd(char *path, int client_fd){
 	return 0;
 }
 
-int handle_port(char* d) 
-{
-
-    // Create a socket
-    int data_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (data_fd < 0) 
-    {
-        printf("Error creating data socket\n");
-        return -1;
-    }
-
-				
-	struct sockaddr_in server_data_addr;
-	bzero(&server_data_addr, sizeof(server_data_addr));
-	socklen_t server_len = sizeof(server_data_addr);
-	server_data_addr.sin_family = AF_INET;
-	server_data_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server_data_addr.sin_port = htons(6000);
-	
-	if (bind(data_fd, (struct sockaddr*) &server_data_addr, sizeof(server_data_addr)) < 0) 
-	{
-        perror("bind failed");
-		exit(-1);
-
-	}
-
-  // Parse the PORT command
-
-
-
-// Extract c and h1 separately
-
-char* h2 = strtok(NULL, ",");
-char* h3 = strtok(NULL, ",");
-char* h4 = strtok(NULL, ",");
-char* p1 = strtok(NULL, ",");
-char* p2 = strtok(NULL, "\r\n");
-
-
-
-char c[5]; // create a new char array to hold the first 4 characters of d
-strncpy(c, d, 4); // copy the first 4 characters of d into c
-c[4] = '\0'; // terminate the string with a null character
-
-char* h1 = strtok(d+4, ","); // start tokenizing from the 5th character of d
-
-h2 = strtok(NULL, ",");
-h3 = strtok(NULL, ",");
-h4 = strtok(NULL, ",");
-p1 = strtok(NULL, ",");
-p2 = strtok(NULL, "\r\n");
-
-
-printf(" ");
-
-    struct sockaddr_in client_data_addr;
-	bzero(&client_data_addr, sizeof(client_data_addr));
-	client_data_addr.sin_family = AF_INET;
-	client_data_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	int port = atoi(p1) * 256 + atoi(p2);
-    client_data_addr.sin_port = htons(port);
-	//client_data_addr.sin_port = htons(6000);
-	int client_len = sizeof(client_data_addr);
-    //declare client stuff before connect
-	//printf("hi its me %d\n",port);
-    //int port = atoi(p1) * 256 + atoi(p2);
-    
-    //data_addr.sin_port = htons(port);
-
-    // if (bind(data_fd, (struct sockaddr*) &client_data_addr,&client_len) < 0) 
-	// {
-	// 	printf("brpoooo\n");
-    //     perror("bind failed");
-	// 	exit(-1);
-
-	// }
-
-//parse the details of the client and use that to connect
-	// convert p1 and p2 to port
-      //port = (p1 * 256) + p2
-	//unsigned int len = sizeof(data_addr);
-    
-    if(connect(data_fd,(struct sockaddr*)&client_data_addr,sizeof(client_data_addr))<0)
-	{
-		perror("connect");
-		exit(-1);
-	}
-
-        
-}
-
-
-
+//list: list out current files etc in dir
 int list(int client_fd){
 
 			int data_fd;
 			//handle_port(data_fd);
-		    int pid = fork();
+
 		    char data_command[256];
 
 		    printf("hello");
 		    send(client_fd, "PORT OK", strlen("PORT OK"), 0);
 		    recv(client_fd, data_command, sizeof(data_command), 0);
-		    if (pid == 0) 
-		    { 	
-		    	handle_port(data_command);
+
+		 
+		    	//handle_port(data_command);
 		        DIR *dir = opendir(".");
 		        if (dir == NULL) 
 		        {
@@ -235,30 +142,25 @@ int list(int client_fd){
 		                continue;  // skip . .. directory --> causes error
 		            }
 		            char printt[256];
-		            snprintf(printt, sizeof(printt), "%s\n", filename->d_name);
+		            snprintf(printt, sizeof(printt), "%s\n", filename->d_name);// send names
 		            snprintf(message, sizeof(message), "200 PORT command successful.\n150 File status okay; about to open. data connection.\n%s", printt);
 		            send(client_fd, message, strlen(message), 0);
 		        }
 		        
 		        closedir(dir);
-		    }    exit(0);
+		        exit(0);
 
 		}
+
 
 
 
 int stor(int client_fd, char *filename)
 {
     
-
-
     int data_fd;
-    int pid = fork();
-
-    if (pid == 0) 
-    {
-        handle_port("");
-        int file_transfer = open(filename, O_CREAT | O_WRONLY, 0644);
+        //handle_port("");
+        int file_transfer = open(filename, O_CREAT | O_WRONLY, 0644); // open file with write acces
         if (file_transfer < 0) 
         {
             char message[256];
@@ -276,7 +178,7 @@ int stor(int client_fd, char *filename)
 
         while (byter > 0) 
         {
-            bytew = write(file_transfer, buffer, byter);
+            bytew = write(file_transfer, buffer, byter); // write data in file
             if (bytew < 0) 
             {
                 char message[256];
@@ -290,26 +192,23 @@ int stor(int client_fd, char *filename)
         close(file_transfer);
         close(data_fd);
         exit(0);
-    } 
+    
 
     char message[256];
     snprintf(message, sizeof(message), "200 PORT command successful.\n150 File status okay; about to open. data connection.\n");
     send(client_fd, message, strlen(message), 0);
     return 0;
-}
+
+		}
 
 
 
 
 int retr(int client_fd, char *filename)
 {
-    int data_fd;
-    int pid = fork();
-
-    if (pid == 0) 
-    {
-        handle_port("");
-        int file_transfer = open(filename, O_RDONLY);
+         int data_fd;
+        //handle_port("");
+        int file_transfer = open(filename, O_RDONLY); // open in read only mode
         if (file_transfer < 0) 
         {
             send(client_fd, "550 No such file or directory", strlen("550 No such file or directory"), 0);
@@ -321,7 +220,7 @@ int retr(int client_fd, char *filename)
         int byter;
         int bytew;
 
-        byter = read(file_transfer, buffer, sizeof(buffer));
+        byter = read(file_transfer, buffer, sizeof(buffer)); // read
 
         while (byter > 0) 
         {
@@ -339,7 +238,7 @@ int retr(int client_fd, char *filename)
         close(file_transfer);
         close(data_fd);
         exit(0);
-    } 
+    
 
     char message[256];
     snprintf(message, sizeof(message), "200 PORT command successful.\n150 File status okay; about to open. data connection.\n");
@@ -347,6 +246,142 @@ int retr(int client_fd, char *filename)
     return 0;
 }
 
+
+// for data socket + forking
+int handle_port(int client_fd, char* d, char code) {
+    
+//printf("hiiiii"); test
+    int pid = fork(); //  fork
+    if (pid == 0) 
+    {
+		    char buffer[256];
+		    recv(client_fd, buffer, sizeof(buffer),0);
+		    // Create data socket
+		    int data_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+		    if (data_fd < 0) 
+		    {
+		        printf("Error creating data socket\n");
+		        return -1;
+		    }
+
+		    int value  = 1;
+			setsockopt(data_fd,SOL_SOCKET,SO_REUSEADDR,&value,sizeof(value)); //&(int){1},sizeof(int)
+			
+
+						
+			struct sockaddr_in server_data_addr;
+			bzero(&server_data_addr, sizeof(server_data_addr));
+			socklen_t server_len = sizeof(server_data_addr);
+			server_data_addr.sin_family = AF_INET;
+			server_data_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+			server_data_addr.sin_port = htons(6000);
+			
+			// check bind
+			if (bind(data_fd, (struct sockaddr*) &server_data_addr, sizeof(server_data_addr)) < 0) 
+			{
+		        perror("bind failed");
+				exit(-1);
+
+			}
+
+		  // Parse the PORT command
+		// Extract c and h1 separately
+		char* h2 = strtok(NULL, ",");
+		char* h3 = strtok(NULL, ",");
+		char* h4 = strtok(NULL, ",");
+		char* p1 = strtok(NULL, ",");
+		char* p2 = strtok(NULL, "\r\n");
+
+		char c[5]; //  char array to hold the first 4 chars of d
+		strncpy(c, d, 4); //  copy first 4 chars of d in c
+		c[4] = '\0'; // add a null character
+
+		char* h1 = strtok(d+4, ","); //  tokenizing from 5th char 
+
+		h2 = strtok(NULL, ",");
+		h3 = strtok(NULL, ",");
+		h4 = strtok(NULL, ",");
+		p1 = strtok(NULL, ",");
+		p2 = strtok(NULL, "\r\n");
+
+
+		printf(" ");
+
+		    struct sockaddr_in client_data_addr;
+			bzero(&client_data_addr, sizeof(client_data_addr));
+			client_data_addr.sin_family = AF_INET;
+			client_data_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+			int port = atoi(p1) * 256 + atoi(p2);
+		    client_data_addr.sin_port = htons(port);
+			//client_data_addr.sin_port = htons(6000);
+			int client_len = sizeof(client_data_addr);
+		    //declare client stuff before connect
+			//printf("hi its me %d\n",port);
+		    //int port = atoi(p1) * 256 + atoi(p2);
+		    
+		    //data_addr.sin_port = htons(port);
+
+		    // if (bind(data_fd, (struct sockaddr*) &client_data_addr,&client_len) < 0) 
+			// {
+			// 	printf("brpoooo\n");
+		    //     perror("bind failed");
+			// 	exit(-1);
+
+			// }
+
+		//parse the details of the client and use that to connect
+			// convert p1 and p2 to port
+		      //port = (p1 * 256) + p2
+			//unsigned int len = sizeof(data_addr);
+		    
+		    // connect
+		    if(connect(data_fd,(struct sockaddr*)&client_data_addr,sizeof(client_data_addr))<0)
+			{
+				perror("connect");
+				exit(-1);
+			}
+
+			// send
+			if (send(data_fd, "sending from server", strlen("sending from server"),0)<0){
+				perror("send");
+				exit(-1);
+			}
+
+			bzero(buffer,sizeof(buffer));
+			recv(client_fd, buffer, sizeof(buffer),0);
+
+			// authentication
+			for (int i = 0; i < num_users; i++) {
+       			 if (userlist[i].is_authenticated==1) {
+
+					if (code == "L"){       // code L for list 
+						list(client_fd);
+					}
+
+					else if (code=="S"){  // S for stor
+						stor(client_fd, d);
+					}
+
+					else if (code=="R"){   // R for retr
+						retr(client_fd, d);
+					}
+
+					close(data_fd);  // close socket
+					exit(0);
+
+				}
+				else {
+					send(client_fd, "503 Login with USER first.\n", strlen("503 Login with USER first.\n"), 0);
+		    		return 0;
+				}
+			}
+		}
+		else{
+			wait(1);
+		}
+		        
+}
 
 int client_port;
 // commands from the client, tokenised reference: https://www.tutorialspoint.com/string-tokenisation-function-in-c
@@ -375,7 +410,7 @@ int commands(int client_sd)
 	    printf("%s\n", data);
 	    printf("%s\n", command);
 
-	    if (command == NULL) {
+	    if (command == NULL) { // if command is not given
 	        send(client_sd, "500 Syntax error, command unrecognized.\n", strlen("500 Syntax error, command unrecognized.\n"), 0);
 	        return 1;
 	    }
@@ -383,7 +418,7 @@ int commands(int client_sd)
 	    //-----USER(username)-----
 	    if (strcmp(command, "USER") == 0) {
 
-	        if (data == NULL) {
+	        if (data == NULL) { // check if username is given or not
 	            send(client_sd, "501 Syntax error in parameters or arguments.\n", strlen("501 Syntax error in parameters or arguments.\n"), 0);
 	            return 1;
 	        }
@@ -399,13 +434,14 @@ int commands(int client_sd)
 	        }
 	        pass(data, client_sd);
 
-
+		//-----cwd---------
 	    } else if (strcmp(command, "CWD") == 0) {
 	        if (data == NULL) {
 	            send(client_sd, "501 Syntax error in parameters or arguments.\n", strlen("501 Syntax error in parameters or arguments.\n"), 0);
 	            return 1;
 	        }
 	        cwd(data, client_sd);
+	    //-----pwd---------
 	    } else if (strcmp(command, "PWD") == 0) {
 	        if (data != NULL) {
 	            send(client_sd, "501 Syntax error in parameters or arguments.\n", strlen("501 Syntax error in parameters or arguments.\n"), 0);
@@ -413,12 +449,16 @@ int commands(int client_sd)
 	        }
 	        pwd(data,client_sd);
 	    } 
-
+        //-----list---------
 	    else if (strcmp(command, "LIST") == 0) 
 	    {	
 	    	//recv(client_sd, clientr, sizeof(clientr), 0);
 	    	//send(client_sd, "ack\n", strlen("ack\n"), 0);	
-			list(client_sd);
+			//list(client_sd);
+			printf("hi");
+			handle_port(client_sd, data, "L");
+
+
 	    } 
 
 	    // else if (strcmp(command, "PORT") == 0) {
@@ -438,20 +478,26 @@ int commands(int client_sd)
 
 	    // } 
 
+        //-----stor---------
 	    else if (strcmp(command, "STOR") == 0) {
 	        if (data == NULL) {
 	            send(client_sd, "501 Syntax error in parameters or arguments.\n", strlen("501 Syntax error in parameters or arguments.\n"), 0);
 	            return 1;
 	        }
-	        stor(client_sd, data);
+	        handle_port(client_sd, data, "S");
+	        //stor(client_sd, data);
 
+		//-----retr---------
 	    } else if (strcmp(command, "RETR") == 0) {
 	        if (data == NULL) {
 	            send(client_sd, "501 Syntax error in parameters or arguments.\n", strlen("501 Syntax error in parameters or arguments.\n"), 0);
 	            return 1;
 	        }
-	        retr(client_sd, data);
+	        handle_port(client_sd, data, "R");
+	        //retr(client_sd, data);
 
+
+        //-----quit---------
 	    } else if (strcmp(command, "QUIT\r\n") == 0) {
 	            send(client_sd, "221 Service closing control connection.\n", strlen("221 Service closing control connection.\n"), 0);
 	            //close(client_sd);
@@ -459,7 +505,7 @@ int commands(int client_sd)
 	            return 1;
 	    
 	    
-	    } else {
+	    } else { //wrong commands
 	        send(client_sd, "500 Syntax error, command unrecognized.\n", strlen("500 Syntax error, command unrecognized.\n"), 0);
 	        return 1;
 	    }
@@ -473,7 +519,7 @@ int commands(int client_sd)
 int main()
 {
 	
-	FILE *fp = fopen("users.txt", "r");
+	FILE *fp = fopen("users.txt", "r"); // for users: username password
     if (fp == NULL) {
         perror("Error opening file");
         exit(1);
@@ -491,7 +537,7 @@ int main()
             user.is_authenticated = 0;
             user.client_fd = -1;
             strcpy(user.address, "");
-            userlist[i++] = user;
+            userlist[i++] = user; // read from file into userlist
             
         }
     }
